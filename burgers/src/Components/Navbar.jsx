99e624e7c6cc707eca7ba7703b2ef8bb1285e4e0
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-12-22 16:25:14 +0300

    add ankors

--- burgers/src/Components/Navbar.jsx
+++ burgers/src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
 
   return (
     <nav className={nav ? "nav active" : nav}>
-      <Link to="main" className="logo">
+      <Link to="main" className="logo" smooth={true} duration={2000}>
         <img src={logo} alt="" />{" "}
       </Link>
       <input className="menu-btn" type="checkbox" id="menu-btn" />
@@ -25,16 +25,24 @@ function Navbar() {
       </label>
       <ul className="menu">
         <li>
-          <Link to="#">Header</Link>
+          <Link to="main" smooth={true} duration={2000}>
+            Header
+          </Link>
         </li>
         <li>
-          <Link to="#">Products</Link>
+          <Link to="products" smooth={true} duration={2000}>
+            Products
+          </Link>
         </li>
         <li>
-          <Link to="#">About</Link>
+          <Link to="about" smooth={true} duration={2000}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="#">Contact</Link>
+          <Link to="contact" smooth={true} duration={2000}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
